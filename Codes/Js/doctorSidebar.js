5484e58e1087152b8215aa5d51a91b3a39c4cd5e
Navoda Perera <e23271@eng.pdn.ac.lk>
2026-07-22 23:44:53 +0530

    Add doctorSidebar.js for sidebar navigation

---
+++ Codes/Js/doctorSidebar.js
@@ -0,0 +1,43 @@
+document.getElementById("sidebar").innerHTML = `
+
+<div class="brand">
+    <h1>MedLogs</h1>
+</div>
+
+<nav>
+
+<a href="DoctorDashboard.html">
+Dashboard
+</a>
+
+<a href="PatientManagement.html">
+Patient Management
+</a>
+
+<a href="CaseManagement.html">
+My Cases
+</a>
+
+<a href="ExaminationForms.html">
+Examination Forms
+</a>
+
+<a href="EvidenceSamples.html">
+Evidence & Samples
+</a>
+
+<a href="Reports.html">
+Reports
+</a>
+
+<a href="Documents.html">
+Documents
+</a>
+
+<a href="Search.html">
+Search
+</a>
+
+</nav>
+
+`;
